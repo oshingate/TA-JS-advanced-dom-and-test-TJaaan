ead21551b00ec70c70aa50254a030b3f61070e11
oshingate <oshingate@gmail.com>
2021-05-05 12:43:10 +0530

    refactor

--- block-BJaait/code/index.js
+++ block-BJaait/code/index.js
@@ -6,7 +6,7 @@ class OneCard {
   }
   createUI() {
     let div = document.createElement("div");
-    div.classList.add("flex", "card");
+    div.classList.add("flex", "card", "draggable");
     let h2 = document.createElement("h2");
     h2.classList.add("flex-80");
     h2.innerText = this.name;
@@ -40,6 +40,61 @@ class AllCards {
     });
   }
 }
+// =======================
+var remove = document.querySelector(".draggable");
+
+function dragStart(e) {
+  this.style.opacity = "0.4";
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/html", this.innerHTML);
+}
+
+function dragEnter(e) {
+  this.classList.add("over");
+}
+
+function dragLeave(e) {
+  e.stopPropagation();
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = "move";
+  return false;
+}
+
+function dragDrop(e) {
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData("text/html");
+  }
+  return false;
+}
+
+function dragEnd(e) {
+  var listItens = document.querySelectorAll(".draggable");
+  [].forEach.call(listItens, function (item) {
+    item.classList.remove("over");
+  });
+  this.style.opacity = "1";
+}
+
+function addEventsDragAndDrop(el) {
+  el.addEventListener("dragstart", dragStart, false);
+  el.addEventListener("dragenter", dragEnter, false);
+  el.addEventListener("dragover", dragOver, false);
+  el.addEventListener("dragleave", dragLeave, false);
+  el.addEventListener("drop", dragDrop, false);
+  el.addEventListener("dragend", dragEnd, false);
+}
+
+var listItens = document.querySelectorAll(".draggable");
+[].forEach.call(listItens, function (item) {
+  addEventsDragAndDrop(item);
+});
+// =======================
 
 let allCards1 = new AllCards(arrayOfCards);
 
